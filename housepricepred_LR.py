88b9f6e7099235ea89cca822286f3ab333360cce
lemongrass99 <lemongrass294@gmail.com>
2024-09-10 06:35:54 +0530

    Update housepricepred_LR.py

--- housepricepred_LR.py
+++ housepricepred_LR.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = pd.read_csv("C:\\Users\\vagis\\OneDrive\\Desktop\\vagish\\Datasets\\homeprices.csv")
+data = pd.read_csv(homeprices csv)
 print(data.info())
 print(data.info())
 print(len(data))
