de0cdf2f7084a82f62d202d66b87db3ba396d0fa
lemongrass99 <lemongrass294@gmail.com>
2024-09-10 06:37:02 +0530

    Update student_studyhour.py

--- student_studyhour.py
+++ student_studyhour.py
@@ -1,30 +1,30 @@
-# import pandas as pd
-# import numpy as np
-# from sklearn.model_selection import train_test_split
-# from sklearn.preprocessing import StandardScaler
-# from sklearn.linear_model import LinearRegression
-# from sklearn.metrics import mean_squared_error
-# df=pd.read_csv("C:\\Users\\vagis\\OneDrive\\Desktop\\vagish\\Datasets\\StudentStudyHour.csv.xls")
-# df
-# y=df['Scores']
-# x=df.drop('Scores',axis=1)
+ import pandas as pd
+ import numpy as np
+ from sklearn.model_selection import train_test_split
+ from sklearn.preprocessing import StandardScaler
+ from sklearn.linear_model import LinearRegression
+ from sklearn.metrics import mean_squared_error
+ df=pd.read_csv(student hour csv)
+ df
+ y=df['Scores']
+ x=df.drop('Scores',axis=1)
 
-# x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.3,random_state=10)
-# print(x_train)
-# print(x_test)
-# scalar=StandardScaler()
-# scalar.fit(x_test)
-# x_train=scalar.transform(x_train)
-# x_test=scalar.transform(x_test)
-# print(x_test)
-# print(x_test)
+ x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.3,random_state=10)
+ print(x_train)
+ print(x_test)
+ scalar=StandardScaler()
+ scalar.fit(x_test)
+ x_train=scalar.transform(x_train)
+ x_test=scalar.transform(x_test)
+ print(x_test)
+ print(x_test)
 
-# lr=LinearRegression
-# model=lr.fit(x_train,y_train)
-# y_pred=model.predict(x_test)
-# df=pd.DataFrame([y_test:y_test, y_pred : y_pred])
-# df
-# print(mean_squared_error(y_test,y_pred))
+ lr=LinearRegression
+ model=lr.fit(x_train,y_train)
+ y_pred=model.predict(x_test)
+ df=pd.DataFrame([y_test:y_test, y_pred : y_pred])
+ df
+ print(mean_squared_error(y_test,y_pred))
 
 
 
